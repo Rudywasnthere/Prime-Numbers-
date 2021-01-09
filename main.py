dab578d3ef551339eaa68318d6010a6915b11e1a
Rudywasnthere <rudybcchs@gmail.com>
2021-01-09 04:40:18 +0000

    almost done :), unless there should be more

--- main.py
+++ main.py
@@ -66,12 +66,13 @@ def plotting(x, y):
   plt.title("Change in ratio of Primes")
   plt.savefig('plot.png', dpi = 72)
   plt.show()
+  print("Your data graph was saved to \'plot.png\'")
 
 
 
 def prime_presenter():
-  lower = int(correct_inputs("lower"))
-  upper = int(correct_inputs("upper"))
+  lower = int(correct_inputs("","lower"))
+  upper = int(correct_inputs("","upper"))
   prime_number_list = []
   for x in range(lower, upper + 1):
     if x%2 == 1 or x==2:
@@ -103,11 +104,34 @@ def prime_data():
     count += step
   print("Getting your image ready...")
   plotting(count_list, prime_count_list)
+  choice = input("Would like to know more information about your graph? (\'yes\'' or \'no\'')")
+  while choice.lower() != "yes" or choice.lower() != "no":
+    choice = input("Tis yes or no mate:\t")
+  if choice == "yes":
+    more_info = 
 
 
   
   
+def main():
+  main_choice = input("I deal with primes, here are my options:\n1: Find the number of primes in a range\n2: Run a graph of prime ratios\nYour choice:\t")
+  play = False
+  while play != True:
+    try:
+      main_choice = int(main_choice)
+      if main_choice in range(1,3):
+        play = True
+      else:
+        main_choice = input("I need a correct choice option:\t")
+    except ValueError:
+      main_choice = input("I need a correct choice option:\t")
   
-prime_data()
+  if main_choice == 1:
+    prime_presenter()
+  
+  if main_choice == 2:
+    prime_data()
+
+main()
 
 
